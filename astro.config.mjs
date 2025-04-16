af18bd431625ac5bf2f9626bee0dae0e862916c8
Dethrer <jhordanzx2020@gmail.com>
2025-04-16 09:21:02 -0500

    A ver

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Dethrer.github.io',
+  base: 'Prueba2',
   vite: {
     plugins: [tailwindcss()]
   }
