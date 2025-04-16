dc199fa7e837edf8b745c1d2debf3a2c301c2109
Dethrer <jhordanzx2020@gmail.com>
2025-04-16 09:25:50 -0500

    Resolve merge conflict in astro.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Dethrer.github.io',
-  base: 'Prueba2',
   vite: {
     plugins: [tailwindcss()]
   }
